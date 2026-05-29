0c8627f5dc72f5663ef0a795cb16a67133569bbc
amirnrn <amirhossein.nourani@gmail.com>
2026-05-29 13:25:17 +0330

    fix some issues

--- src/shared/scripts/generate-manifests.js
+++ src/shared/scripts/generate-manifests.js
@@ -3,14 +3,10 @@ import path from "path";
 
 const baseDir = path.join(process.cwd(), "src/app/applications");
 const outputDir = path.join(process.cwd(), "src/app");
-
 const outputFile = path.join(outputDir, "generated-manifests.ts");
 
 const dirs = fs.readdirSync(baseDir);
-
-// Add quotes around each directory name
 const quotedDirs = dirs.map((dir) => `"${dir}"`);
-
 const content = `export const manifests = [${quotedDirs.join(", ")}];`;
 
 fs.writeFileSync(outputFile, content);
